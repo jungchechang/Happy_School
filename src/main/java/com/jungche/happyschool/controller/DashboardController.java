ae68fdaa3cff883a3a6309313b0a251654957e07
JungCheChang <jungche0408@gmail.com>
2024-04-03 18:43:02 -0700

    Added dashboard page

---
+++ src/main/java/com/jungche/happyschool/controller/DashboardController.java
@@ -0,0 +1,19 @@
+package com.jungche.happyschool.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DashboardController {
+
+    @RequestMapping("/dashboard")
+    public String displayDashboardPage(Model model, Authentication authentication) {
+        model.addAttribute("username", authentication.getName());
+        model.addAttribute("roles", authentication.getAuthorities());
+
+        return "dashboard.html";
+
+    }
+}
