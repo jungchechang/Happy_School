d0eba729b319b497c80be49eee3d65a50cf93b2f
JungCheChang <jungche0408@gmail.com>
2024-04-07 16:15:25 -0700

    Update business logic for holiday controller

--- src/main/java/com/jungche/happyschool/controller/HolidaysController.java
+++ src/main/java/com/jungche/happyschool/controller/HolidaysController.java
@@ -1,7 +1,9 @@
 package com.jungche.happyschool.controller;
 
 import com.jungche.happyschool.model.Holiday;
+import com.jungche.happyschool.repository.HolidaysRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 @Controller
 public class HolidaysController {
 
+    @Autowired
+    private HolidaysRepository holidaysRepository;
     @GetMapping("/holidays/{display}")
     public String displayHolidays(Model model, @PathVariable String display) {
         model.addAttribute("display", display);
@@ -27,20 +32,14 @@ public class HolidaysController {
         }else {
             model.addAttribute("festival", true);
         }
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+        Iterable<Holiday> holidays = holidaysRepository.findAll();
+        List<Holiday> holidayList = StreamSupport
+                .stream(holidays.spliterator(), false)
+                .collect(Collectors.toList());
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
             model.addAttribute(type.toString(),
-                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
+                    (holidayList.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
         }
         return "holidays.html";
     }
