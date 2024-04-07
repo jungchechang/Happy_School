7b5ce5ebc10ab3a20dde4a1ad49a4a086b33c35b
JungCheChang <jungche0408@gmail.com>
2024-04-07 16:13:56 -0700

    Added @EnableJpaRepositories and @EntityScan for activating spring data JPA

--- src/main/java/com/jungche/happyschool/HappyschoolApplication.java
+++ src/main/java/com/jungche/happyschool/HappyschoolApplication.java
@@ -2,8 +2,12 @@ package com.jungche.happyschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.jungche.happyschool.repository")
+@EntityScan("com.jungche.happyschool.model")
 public class HappyschoolApplication {
 
 	public static void main(String[] args) {
