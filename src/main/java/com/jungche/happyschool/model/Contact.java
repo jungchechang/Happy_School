9da2c2dcfa8d724b49871c9ab476a62eca654c7e
JungCheChang <jungche0408@gmail.com>
2024-04-06 17:27:18 -0700

    Update Model to JPA Entity

--- src/main/java/com/jungche/happyschool/model/Contact.java
+++ src/main/java/com/jungche/happyschool/model/Contact.java
@@ -1,10 +1,12 @@
 package com.jungche.happyschool.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 /*
 @Data annotation is provided by Lombok library which generates getter, setter,
@@ -12,7 +14,15 @@ equals(), hashCode(), toString() methods & Constructor at compile time.
 This makes our code short and clean.
 * */
 @Data
-public class Contact {
+@Entity
+@Table(name = "contact_msg")
+public class Contact extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    @Column(name = "contact_id")
+    private int contactId;
+
     @NotBlank(message="Name must not be blank")
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
@@ -32,4 +42,6 @@ public class Contact {
     @NotBlank(message="Message must not be blank")
     @Size(min=10, message="Message must be at least 10 characters long")
     private String message;
+
+    private String status;
 }
