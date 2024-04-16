9ee505614c91cbb0addc8ffc31c9bf2faa1b301b
JungCheChang <jungche0408@gmail.com>
2024-04-16 10:35:03 -0700

    Added hBCryptPasswordEncoder

--- src/main/java/com/jungche/happyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/jungche/happyschool/config/ProjectSecurityConfig.java
@@ -4,9 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +16,7 @@ public class ProjectSecurityConfig {
 
         http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg"))
                 .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
+                        .requestMatchers("/displayProfile").authenticated()
                         .requestMatchers("/", "/home").permitAll()
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/displayMessages").hasRole("ADMIN")
@@ -37,5 +37,10 @@ public class ProjectSecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/jungche/happyschool/security/HappySchoolUsernamePwdAuthenticationProvider.java
+++ src/main/java/com/jungche/happyschool/security/HappySchoolUsernamePwdAuthenticationProvider.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -21,6 +22,8 @@ public class HappySchoolUsernamePwdAuthenticationProvider implements Authenticat
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public Authentication authenticate(Authentication authentication)
             throws AuthenticationException {
@@ -28,9 +31,9 @@ public class HappySchoolUsernamePwdAuthenticationProvider implements Authenticat
         String pwd = authentication.getCredentials().toString();
         Person person = personRepository.readByEmail(email);
         if(null != person && person.getPersonId()>0 &&
-                pwd.equals(person.getPwd())){
+                passwordEncoder.matches(pwd,person.getPwd())){
             return new UsernamePasswordAuthenticationToken(
-                    person.getName(), pwd, getGrantedAuthorities(person.getRoles()));
+                    email, null, getGrantedAuthorities(person.getRoles()));
         }else{
             throw new BadCredentialsException("Invalid credentials!");
         }
