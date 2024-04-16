59912a1f34172b89ee9cdcdac025b7749561d07e
JungCheChang <jungche0408@gmail.com>
2024-04-15 17:33:01 -0700

    Added PersonRepository

---
+++ src/main/java/com/jungche/happyschool/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.jungche.happyschool.repository;
+
+import com.jungche.happyschool.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+}
