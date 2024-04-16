1e0fbac2af3f8b98682373de57a32bbed161ac34
JungCheChang <jungche0408@gmail.com>
2024-04-15 17:33:09 -0700

    Added PersonService

---
+++ src/main/java/com/jungche/happyschool/service/PersonService.java
@@ -0,0 +1,31 @@
+package com.jungche.happyschool.service;
+
+import com.jungche.happyschool.constants.SchoolConstants;
+import com.jungche.happyschool.model.Person;
+import com.jungche.happyschool.model.Roles;
+import com.jungche.happyschool.repository.PersonRepository;
+import com.jungche.happyschool.repository.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private RolesRepository rolesRepository;
+
+    public boolean createNewPerson(Person person){
+        boolean isSaved = false;
+        Roles role = rolesRepository.getByRoleName(SchoolConstants.STUDENT_ROLE);
+        person.setRoles(role);
+        person = personRepository.save(person);
+        if (null != person && person.getPersonId() > 0)
+        {
+            isSaved = true;
+        }
+        return isSaved;
+    }
+}
