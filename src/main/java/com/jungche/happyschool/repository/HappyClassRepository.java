237c41ebba7deb24bfa8f84b261defb9ca60536a
JungCheChang <jungche0408@gmail.com>
2024-04-16 11:20:11 -0700

    Added HappyClassRepository

---
+++ src/main/java/com/jungche/happyschool/repository/HappyClassRepository.java
@@ -0,0 +1,7 @@
+package com.jungche.happyschool.repository;
+
+import com.jungche.happyschool.model.HappyClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HappyClassRepository extends JpaRepository<HappyClass, Integer> {
+}
